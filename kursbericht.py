8acb729658ac4de1b19d3e11541471fb0c8064b5
Isabelle <70333084+isathecatgirl@users.noreply.github.com>
2025-02-21 14:18:45 +0100

    Update kursbericht.py

--- kursbericht.py
+++ kursbericht.py
@@ -6,39 +6,44 @@ class Lehrer:
     def erklaeren(_):
         pass
 
+ziele = {
+    "aus dem Kurs": 0,
+    "in den Unterricht": 1
+}
+
 class Schuelerli:
     def __init__(self, name):
         self.name = name
         self.in_kurs = True
 
-    def abwaehlen(schuelerli):
-        schuelerli.in_kurs = False
-
-    def unterricht_besuchen(schuelerli):
-        print(schuelerli.name + " ist da")
+    def move(schuelerli, ziel):
+        if (ziel == ziele["aus dem Kurs"]):
+            schuelerli.in_kurs = False
 
 bernd = Lehrer()
 
+# Too repetitive?
 # TODO: make this look nicer / remove?
-max = Schuelerli("Max")
-max.abwaehlen()
-jan = Schuelerli("Jan")
-jan.abwaehlen()
-carolin = Schuelerli("Carolin")
-carolin.abwaehlen()
+abwaehler = [
+    Schuelerli("Max"),
+    Schuelerli("Jan"),
+    Schuelerli("Carolin")
+]
+
+for schuelerli in abwaehler:
+    schuelerli.move(ziele["aus dem Kurs"])
 
 # Needs approval; TODO: do async if not too disgusting?
-wand_reihe = [
+wandreihe = [
     Schuelerli("Janis"),
     Schuelerli("Andreas"),
     Schuelerli("Joel"),
     Schuelerli("Elias")
 ]
 
-for schuelerli in wand_reihe:
-    if schuelerli.name == "Elias" and random.randint(1, 10) < 10:
+for schuelerli in wandreihe:
+    if schuelerli.name == "Elias" and random.randint(1, 10) > 9:
         continue
 
     time.sleep(random.randint(180, 420))
-    schuelerli.unterricht_besuchen()
-    
+    schuelerli.move(ziele["in den Unterricht"])
